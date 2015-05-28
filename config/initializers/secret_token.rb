da31448098b357cd913068f65b6c165d299a98c2
rowland rudolf <rowlandrudolf@rowlands-MacBook-Pro.local>
2015-05-27 21:49:29 -0400

    Rails 4.2.0, Rails-api, Postgresql, Puma 2.11.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ThenitespotApi::Application.config.secret_token = '4d69743ecbc108702fa596ff9cc912dee52a61eaa2266f1d00b7c7d167ec153806ba87df1605f25cf973a7348366235d06434cf72dcc009eb28bddce7d18b18e'
